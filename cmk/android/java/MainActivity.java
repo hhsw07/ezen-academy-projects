797d7d870de08a6bcc9abeacafc09dd13e6e8bea
mkeeeeey <60504392+mkeeeeey@users.noreply.github.com>
2020-02-19 16:52:40 +0900

    Create MainActivity.java

---
+++ cmk/android/java/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pet_care;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+
+
+
+    }
+
+}
