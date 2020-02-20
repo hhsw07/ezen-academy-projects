b5e0675248ab80f47ba7ef0afc071144cf9ddffa
jhgyu <44195846+jhgyu@users.noreply.github.com>
2020-02-20 15:47:08 +0900

    android

---
+++ Android/app/src/main/java/com/example/hellopetsitter/loginActivity.java
@@ -0,0 +1,48 @@
+package com.example.hellopetsitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button goj = (Button)findViewById(R.id.go_join);
+        Button fi = (Button)findViewById(R.id.find_id);
+        Button fp = (Button)findViewById(R.id.find_pass);
+
+        goj.setOnClickListener(gjListen);
+        fi.setOnClickListener(fiListen);
+        fp.setOnClickListener(fpListen);
+    }
+    View.OnClickListener fiListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent fi = new Intent(loginActivity.this, findIdActivity.class);
+            startActivity(fi);
+        }
+    };
+    View.OnClickListener fpListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent fp = new Intent(loginActivity.this, findPassActivity.class);
+            startActivity(fp);
+        }
+    };
+
+    View.OnClickListener gjListen = new View.OnClickListener() {
+        Intent gj;
+        @Override
+        public void onClick(View v) {
+            gj = new Intent(loginActivity.this, termActivity.class);
+            startActivity(gj);
+        }
+    };
+}
---
+++ Android/app/src/main/java/com/example/hellopetsitter/termActivity.java
@@ -0,0 +1,38 @@
+package com.example.hellopetsitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class termActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.term);
+
+        Button login = (Button)findViewById(R.id.go_login);
+        Button next =(Button)findViewById(R.id.nextjoin);
+
+        login.setOnClickListener(glListen);
+        next.setOnClickListener(njListener);
+    }
+    View.OnClickListener glListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent gl = new Intent(termActivity.this, loginActivity.class);
+            startActivity(gl);
+        }
+    };
+    View.OnClickListener njListener = new View.OnClickListener() {
+        Intent nj;
+        @Override
+        public void onClick(View v) {
+            nj = new Intent(termActivity.this, joinActivity.class);
+            startActivity(nj);
+        }
+    };
+}
