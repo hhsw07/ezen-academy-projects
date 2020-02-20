a74fe786776c2703e3eb0798d88c0ec53674f2a6
hhsw07 <60540555+hhsw07@users.noreply.github.com>
2020-02-20 13:16:05 +0900

    document/hsw폴더 정리

---
+++ WebContent/hsw/android/java/qna.java
@@ -0,0 +1,60 @@
+package com.example.ezenpetsitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class qna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pro03_qna);
+
+        TextView notice = (TextView)findViewById(R.id.title_notice);
+        TextView faq = (TextView)findViewById(R.id.title_faq);
+        TextView qna = (TextView)findViewById(R.id.title_qna);
+
+        notice.setOnClickListener(notClick);
+        faq.setOnClickListener(faqClick);
+        qna.setOnClickListener(qnaClick);
+    }
+    View.OnClickListener notClick = new View.OnClickListener() {
+        Intent int01;
+
+        // 이 클래스를 통해서, 특정한 이벤트 click을 했을 때 처리할 내용을 기술한다.
+        @Override
+        public void onClick(View v) {
+            int01 = new Intent(getApplicationContext(),notice.class);
+            // 해당 activity 로딩 시작.
+            startActivity(int01);
+        }
+    };
+    View.OnClickListener faqClick = new View.OnClickListener() {
+        Intent int02;
+
+        // 이 클래스를 통해서, 특정한 이벤트 click을 했을 때 처리할 내용을 기술한다.
+        @Override
+        public void onClick(View v) {
+            int02 = new Intent(getApplicationContext(),faq.class);
+            startActivity(int02);
+        }
+    };
+    View.OnClickListener qnaClick = new View.OnClickListener() {
+        Intent int03;
+
+        // 이 클래스를 통해서, 특정한 이벤트 click을 했을 때 처리할 내용을 기술한다.
+        @Override
+        public void onClick(View v) {
+            int03 = new Intent(getApplicationContext(),qna.class);
+            // 해당 activity 로딩 시작.
+            startActivity(int03);
+        }
+    };
+}
+
