3649bd8e75d2b3aa5d10098070c9d4369e142bc1
mkeeeeey <60504392+mkeeeeey@users.noreply.github.com>
2020-02-20 16:40:40 +0900

    android

--- Android/app/src/main/java/com/example/hellopetsitter/MainActivity.java
+++ Android/app/src/main/java/com/example/hellopetsitter/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hellopetsitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button)findViewById(R.id.go_login);
+        Button cs = (Button)findViewById(R.id.go_cs);
+        Button fs = (Button)findViewById(R.id.find_sitter);
+
+        login.setOnClickListener(glListen);
+        cs.setOnClickListener(csListen);
     }
+    View.OnClickListener glListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent gl = new Intent(MainActivity.this, loginActivity.class);
+            startActivity(gl);
+        }
+    };
+    View.OnClickListener csListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent csl = new Intent(MainActivity.this, notice.class);
+            startActivity(csl);
+        }
+    };
+    View.OnClickListener fsListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent fsl = new Intent(MainActivity.this, findsitter.class);
+            startActivity(fsl);
+        }
+    };
 }
---
+++ Android/app/src/main/java/com/example/hellopetsitter/findsitter.java
@@ -0,0 +1,14 @@
+package com.example.hellopetsitter;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class findsitter extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.findsitter);
+    }
+}
