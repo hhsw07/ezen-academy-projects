7e4d6c2225a44d7605f1693118207edae547e3c6
skxowns19 <60504099+skxowns19@users.noreply.github.com>
2020-02-26 15:26:15 +0900

    ud

--- Android/app/src/main/java/com/example/hellopetsitter/sitterpageActivity.java
+++ Android/app/src/main/java/com/example/hellopetsitter/sitterpageActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,10 +19,13 @@ public class sitterpageActivity extends AppCompatActivity {
         Button sv = (Button) findViewById(R.id.add_sv);
         Button ac = (Button) findViewById(R.id.add_ac);
         Button gm = (Button) findViewById(R.id.go_my);
+        ImageView gma = (ImageView) findViewById(R.id.go_main);
 
         sv.setOnClickListener(svListen);
         ac.setOnClickListener(acListen);
         gm.setOnClickListener(gmListen);
+        gma.setOnClickListener(gmaListen);
+
 
     }
 
@@ -44,6 +48,13 @@ public class sitterpageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gml = new Intent(sitterpageActivity.this,mypageActivity.class);
                 startActivity(gml);
+            }
+        };
+        View.OnClickListener gmaListen = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gmal = new Intent(sitterpageActivity.this,MainActivity.class);
+                startActivity(gmal);
             }
         };
     }
