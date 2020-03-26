23652bd39c3f4c5d0cdc2528984ea6aa18642b1b
skxowns19 <60504099+skxowns19@users.noreply.github.com>
2020-03-26 18:30:24 +0900

    ud

--- Android/app/src/main/java/com/example/hobbyfactory/Cpayment.java
+++ Android/app/src/main/java/com/example/hobbyfactory/Cpayment.java
@@ -1,6 +1,9 @@
 package com.example.hobbyfactory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,16 @@ public class Cpayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.classpayment);
+        Button gomcp = (Button) findViewById(R.id.go_mcp);
+
+        gomcp.setOnClickListener(gmcpListen);
     }
+
+    View.OnClickListener gmcpListen = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent gcd= new Intent(Cpayment.this, MainActivity.class );
+            startActivity(gcd);
+        }
+    };
 }
--- Android/app/src/main/java/com/example/hobbyfactory/Ppayment.java
+++ Android/app/src/main/java/com/example/hobbyfactory/Ppayment.java
@@ -1,6 +1,9 @@
 package com.example.hobbyfactory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,17 @@ public class Ppayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pdtpayment);
+
+        Button gomsp = (Button) findViewById(R.id.go_msp);
+
+        gomsp.setOnClickListener(gmspListen);
     }
+
+    View.OnClickListener gmspListen = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Intent gcd= new Intent(Ppayment.this, MainActivity.class );
+            startActivity(gcd);
+        }
+    };
 }
