dde245a7a9f6f01d3a4a752278e74c909cb2bf10
skxowns19 <60504099+skxowns19@users.noreply.github.com>
2020-04-16 15:44:11 +0900

    0416

---
+++ src/khj/A01_loginCtrl.java
@@ -0,0 +1,32 @@
+package khj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class A01_loginCtrl
+ */
+@WebServlet("/A01_loginCtrl")
+public class A01_loginCtrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public A01_loginCtrl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
