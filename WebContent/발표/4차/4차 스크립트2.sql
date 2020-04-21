e410f05cbb86ea060e4044ec8f11c436681ed02f
jhgyu <44195846+jhgyu@users.noreply.github.com>
2020-04-21 16:15:29 +0900

    2020-04-21

--- WebContent/발표/4차/4차 스크립트2.sql
+++ WebContent/발표/4차/4차 스크립트2.sql
@@ -760,7 +760,11 @@ CREATE SEQUENCE p5_review_seq
 START WITH 1
 INCREMENT BY 1
 ;
-INSERT INTO p5_review VALUES ();
+INSERT INTO p5_review VALUES (p5_review_seq.nextval,'ezen01','PC냥이 감사의 마음으로 리뷰남겨봅니다.','1.배송:빨랐습니다. 일요일주문후 수요일에 왔으니 뭐 아주 만족합니다\n 포장상태도 3000원 추가하니 정말 안전하게 잘 포장해주셨습니다.\n2.원래는 견적에서 rx570(벌크)를 포함했으나 보라컴측에서 친절하게 바로 다음날\n 재고가 없다고 하시면서 어떻게 진행해 드리냐고 물어봐주셔서 없이 해달라고했는데\n 오늘 택배온거 보니 pci선이랑 따로 나와있게 센스있게 선정리 너무 잘해주셔서 감동했습니다 ^^\n (덕분에 그래픽카드 첨껴보는 저도 손쉽게 꼈습니다)\n3.순조롭게 윈도우까지 깔고 파스 돌려보니 점수 생각만큼 잘 나와줬네요.\n\n정말 감사합니다. 암드가 이제 대세가 될 조짐이 보이긴하지만 그래도 현시점에선\n아직 게임으론 i9100f+rx570이 킹성비조합이라 매우 만족스럽네요\n다음에도 컴퓨터 맞추거나 할땐 PC냥이 1순위로 찾겠습니다.\n친구들한테도 추천도 할거구요.. 번창하세요~',to_date('2019-05-30','YYYY-MM-DD'));
+INSERT INTO p5_review VALUES (p5_review_seq.nextval,'ezen02','솔직리뷰','인터넷 이곳저곳 찾아다니다가 우연히 알게됬습니다. 컴퓨터를 사기 전에 인터넷 후기글을 봤는데 다른 미니샵들이 후기가 너무 좋지않더군요... 그래도 다른곳보다 싸서 피씨톡이란 미니샵을 믿고 구매했는데 너무너무 만족합니다. 사장님도 너무 친절하시고 톡톡문의 보내면 바로바로 답장해주시고 그리고 저번주 목요일날 시켰는데 주말에 단순 변심때문에 케이스와 쿨러 교체가 가능하냐고 조심스럽게 여쭤봤는데 바로 알겠다하시고 떻하니 오늘 교체된 부품으로 배송 왔습니다.한가지 아쉬운건 컴터를 주문할때 글카는 집에 있는걸 쓰려고 6+2핀을 미리 빼달라고 부탁드렸었는데 깜빡하셨는지 안빠져있더라구요. 근데 제가 너무 귀찮게 정신없이 톡톡을 보내서 이해는 됬습니다...ㅋㅋㅋ 그래서 6+2핀을 빼려고 옆에를 열었는데 선정리가 너무 깔끔해서 놀랐습니다.만약 나중에 컴터를 또 시키게되면 여기서 또 살거같습니다. 친구컴이랑 제컴이랑 총 2개시켰는데 친구컴도 한번 구경하러가보려구요 ㅋㅋ수고하세요~',to_date('2019-10-16','YYYY-MM-DD'));
+INSERT INTO p5_review VALUES (p5_review_seq.nextval,'ezen03','물건 왔습니다~','생애 첨으로 본체를 구입했습니다.^^\n모니터가 며칠 있다가 오다보니 TV에 연결해서 셋팅 했습니다. ^^\n선정리 만족, 배송기간 만족\n많이 파세요~~^^',to_date('2020-03-10','YYYY-MM-DD'));
+INSERT INTO p5_review VALUES (p5_review_seq.nextval,'ezen04','본체 잘받았습니다~','본체 받은건 저번주에 받았는데 본가에 받아서 일요일날 집에 설치후 작동확인 했고 잘됩니다.\n배송올때 박스 포장이나 에어캡도 안전하게 잘되있고 선정리도 깔끔히 잘되있습니다.\n택배박스에 기사분들한테 조심히 다뤄달라는 문구가 있는데 마음에듭니다ㅎㅎ',to_date('2020-03-17','YYYY-MM-DD'));
+INSERT INTO p5_review VALUES (p5_review_seq.nextval,'ezen05','받은지 좀 됐지만 이제야 후기 ㅎ','받은지 좀 지났지만 이제야 후기 씁니다.\n현재까지 고장하나없이 아주 잘 사용하고 있습니다 ..ㅎㅎ 원래 후기 남기는 성격이 아니라서 \n깜빡하고 있다가 주변에서 다른 곳 컴퓨터 맞추다가 몇주째 교환만 계속 하는 모습보고 문득 생각나서 \n후기씁니다 ..ㅎㅎ 조립도 빠르고 배송도 몇일만에 바로 오고 케이스 전부 깨끗하고 고장하나 없이 잘 와서\n사용중이에요. 다음에도 기회가 된다면 여기에서 맞출거 같아요 ㅎ',to_date('2020-04-01','YYYY-MM-DD'));
 SELECT * FROM p5_review;
 --------------------------------------
 /* 상품후기댓글 */
@@ -805,7 +809,11 @@ CREATE SEQUENCE p5_revcomm_seq
 START WITH 1
 INCREMENT BY 1
 ;
-INSERT INTO p5_revcomm VALUES ();
+INSERT INTO p5_revcomm VALUES (p5_revcomm_seq.nextval, 1, 'admin', '좋은 리뷰~ 정말 감사합니다.\n새로운 컴퓨터와 함께 좋은 날들 되시기를 바랍니다.\n감사합니다.',to_date('2019-05-31','YYYY-MM-DD'));
+INSERT INTO p5_revcomm VALUES (p5_revcomm_seq.nextval, 2, 'admin', '안녕하세요. 통합 고객만족센터입니다.\n우선 그래픽카드 핀 못 빼놓은 것은 죄송합니다.\n최근 인력 교체가 발생하면서 회사내에서 내용을 놓친 것 같습니다.\n또한 너무나도 좋은 리뷰, 정말 감사드립니다.\n이런 리뷰를 보면 일하는 것이 행복해집니다. ^^\n앞으로 더 노력하는 회사가 되도록 하겠습니다.\n감사합니다.',to_date('2019-10-16','YYYY-MM-DD'));
+INSERT INTO p5_revcomm VALUES (p5_revcomm_seq.nextval, 3, 'admin', '안녕하세요. 통합 고객만족센터입니다.\n좋은 리뷰, 너무 감사합니다.\n오랫동안 잘 사용하시기 바랍니다.\n감사합니다. ^^',to_date('2020-03-11','YYYY-MM-DD'));
+INSERT INTO p5_revcomm VALUES (p5_revcomm_seq.nextval, 4, 'admin', '안녕하세요. 통합 고객만족센터입니다.\n요즘처럼 어려운 때에 좋은 리뷰를 써주셔서 진심으로 감사드립니다.\n정말 큰 힘이 됩니다.\n건승하시기를 바랍니다.^^',to_date('2020-03-18','YYYY-MM-DD'));
+INSERT INTO p5_revcomm VALUES (p5_revcomm_seq.nextval, 5, 'admin', '좋은 리뷰 너무나 감사드립니다. ^^\n최선을 다하고 있습니다.\n코로나19 때문에 많이 어려운 상황인데\n건승하시기를 바랍니다.\n감사합니다.',to_date('2020-04-12','YYYY-MM-DD'));
 SELECT * FROM p5_revcomm;
 --------------------------------------
 /* 문의사항 */
@@ -840,7 +848,12 @@ CREATE SEQUENCE p5_question_seq
 START WITH 1
 INCREMENT BY 1
 ;
-INSERT INTO p5_question VALUES ();
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen01', '박스포장 되서 배송되는거맞죠??', '010 7709 8710으로 주문한 사람입니다.', to_date('2018-11-26','YYYY-MM-DD'));
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen02', '주소좀', '대충 거리 적당하면 행복쇼핑에서 결제만 하고 직접 찾으러 가고싶은데 주소좀 알 수 있을까요?', to_date('2019-01-03','YYYY-MM-DD'));
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen03', '재고확인 부탁드립니다.', 'CPU AMD 라이젠 5 3600 (마티스) (정품)\n메인보드 ASUS TUF B450M-PRO GAMING STCOM\n메모리 삼성전자 DDR4 8G PC4-21300 (정품)  2개\n하드디스크 Western Digital WD 1TB BLUE WD10EZEX (SATA3/7200/64M)\nSSD 마이크론 Crucial MX500 대원CTS (500GB)\n그래픽카드 갤럭시 GALAX 지포스 RTX 2070 SUPER EX BLACK OC D6 8GB\n케이스 darkFlash G-CLASS 500 RGB (화이트)\n파워 Antec NE650C TUF 80PLUS BRONZE \n쿨러/튜닝 JONSBO CR-1000 ARGB (BLACK)\n소프트웨어 Microsoft Windows 10 Home (처음사용자용 한글)\n\n재고 확인부탁드립니다.', to_date('2019-10-21','YYYY-MM-DD'));
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen04', '질문요', '윈도우10 없는데 무료로 깔아주시나요?', to_date('2020-01-03','YYYY-MM-DD'));
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen05', '조립하지않고 부품만 배송시 지금 주문하면 이번주가능한가요?', '조립하지 않고 부품만 배송시 지금 주문하면 이번주 수령 가능한가요?', to_date('2020-01-30','YYYY-MM-DD'));
+INSERT INTO p5_question VALUES (p5_question_seq.nextval, 'ezen01', '쿨러 AEROCOOL Cylon 4 랑 인텔쿨러랑 조립법똑같나요?', '일반적인 인텔쿨러와 동일하게 하면 될까요?', to_date('2020-04-20','YYYY-MM-DD'));
 SELECT * FROM p5_question;
 --------------------------------------
 /* 문의사항댓글 */
@@ -885,7 +898,13 @@ CREATE SEQUENCE p5_quecomm_seq
 START WITH 1
 INCREMENT BY 1
 ;
-INSERT INTO p5_quecomm VALUES ();
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,1,'admin','네 안녕하세요.\n대박스 안전포장추가입금 확인했습니다. 대박스 안전포장으로 안전하게 발송하겠습니다.',to_date('2018-11-27','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,2,'admin','안녕하세요. 통합고객만족센터입니다.\n(매장주소: 서울특별시 종로구 미려빌딩 6층)입니다.\n방문수령은 주문후에 수령시간 조정이 필요합니다.\n주문후에 판매처로 꼭 방문수령 문의주세요.\n감사합니다.',to_date('2019-01-04','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,3,'admin','안녕하세요. 문의 주셔서 감사합니다.\n현재 문의주신 VGA 는 일시품절로 보이며\n\n갤럭시 GALAX 지포스 RTX 2070 SUPER EX WHITE OC D6 8GB\nPALIT 지포스 RTX 2070 SUPER JS D6 8GB\n기가바이트 지포스 RTX 2070 SUPER Gaming OC D6 8GB\n이엠텍 HV 지포스 RTX 2070 SUPER BLACK MONSTER OC D6 8GB\nASUS DUAL 지포스 RTX 2070 SUPER O8G EVO D6 8GB\n이엠텍 제논 지포스 RTX 2070 SUPER TURBO JET OC D6 8GB\n등이 재고 가능한것으로 보입니다.\n\n구매에 참고 부탁드립니다.\n감사합니다~',to_date('2019-10-22','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,4,'admin','정품 추가의 경우에만 설치가 가능합니다. 감사합니다.',to_date('2020-01-04','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,5,'admin','죄송합니다만 현재 주문량이 밀려있어 월요일에 가능할 것으로 보입니다. 감사합니다.',to_date('2020-01-31','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,6,'admin','안녕하세요. 말씀주신 쿨러 제작은 인텔 순정쿨러와는 조립 방법이 다릅니다.\n보드를 들어낸 상태로 하셔야 합니다.\n난이도가 조금 있습니다!!',to_date('2020-04-21','YYYY-MM-DD'));
+INSERT INTO p5_quecomm VALUES (p5_quecomm_seq.nextval,6,'ezen01','감사합니다.',to_date('2020-04-22','YYYY-MM-DD'));
 SELECT * FROM p5_quecomm;
 --------------------------------------
 /* 공지사항 */
@@ -910,6 +929,9 @@ CREATE SEQUENCE p5_notice_seq
 START WITH 1
 INCREMENT BY 1
 ;
-INSERT INTO p5_notice VALUES ();
+INSERT INTO p5_notice VALUES (p5_notice_seq.nextval,'고객만족센터 이용 안내★필독','이 곳을 통해 컴퓨터 견적을 상담받으실 수 있으며,\n각종 컴퓨터 관련 정보와 팁을 공유할 수 있고\n구매하신 컴퓨터의 불편한 사항에 대해서 불편 접수를 하실 수 있습니다.\n★ 각 제품마다 A/S에 대한 약속이 다르게 이루어지고 있습니다.\nA/S 접수 전에 해당 컴퓨터의 A/S 의 기간과 종류에 대해 자세히 확인해주시기 바랍니다.',to_date('2018-11-26','YYYY-MM-DD'));
+INSERT INTO p5_notice VALUES (p5_notice_seq.nextval,'글 작성 이용수칙을 지켜주세요','해당 글을 삭제할 수 있습니다.\n(1) 악의적으로 판매를 방해하는 글  (글 삭제 및 활동 정지)\n(2) 같은 내용을 반복해서 올리는 글 (글 삭제 및 활동 정지)\n(3) 다른 고객님들에게 안좋은 오해를 줄 수 있는 제목을 가진 글 (글 삭제 및 활동 정지)\n(4) 불편 내용을 작성하고, 제품을 반품하는 경우 (글 삭제)\n(5) PC냥이에서 구매하지 않은 제품에 대한 불만내용(혹은 AS문제)을\n 마치 PC냥이에서 구매한 것처럼 올리는 경우 (글 삭제 및 활동 정지)\n(6) 타 제작 업체의 링크를 포함한 경우 (글 삭제)\n(7) 해당 내용과 관계없는 글에 답글/댓글을 남긴 경우 (글 삭제)\n\n※ (1)~(3) 과 같이 판매에 방해되는 행동을 하는 경우에는\n 이 문제가 회사 전체에 가져다주는 영향력이 워낙 크기 때문에 삭제할 수 밖에 없습니다.\n※ (4) 와 같은 경우는 반품을 통해 해당 문제가 완전하게 해결이 된 것이며,\n 더이상 본 센터의 고객이라고 볼 수 없기 때문에 해당 글은 삭제됩니다.\n (이 경우에는 구매 고객이 아닌 것으로 간주합니다.)\n※ 모든 글 삭제 및 활동정지는 죄송하게도 경고없이 처리할 수 밖에 없음을 양해 부탁드립니다.\n\n★ 서비스에 불만족하는 일반적인 글을 삭제하지는 않습니다.',to_date('2018-11-27','YYYY-MM-DD'));
+INSERT INTO p5_notice VALUES (p5_notice_seq.nextval,'AMD CPU 사용 시 정품 쿨러 장착을 권장합니다.','안녕하세요.\nPC냥이의 컴퓨터 통합 고객만족센터입니다.\n\nAMD CPU 쿨러에 대한 간단한 안내입니다.\n\nAMD CPU 의 경우,\nAMD 정품 쿨러가 아닌 다른 회사가 제작한 쿨러(일명 사제 쿨러)를 장착할 경우\n특유의 핀 휨 현상이 일어날 수 있으니 정품 쿨러를 사용하는 것을 추천합니다.\n★ 쿨러의 무게가 무거울수록 해당 문제가 더 많이 나타나는 것으로 확인됩니다.\n★ 핀 휨 현상이 발생한 CPU 는 무상 A/S 처리가 되지 않습니다.\n★ 특히 택배로 배송을 받는 경우에는 더욱 주의가 필요합니다.\n\n참고하여 좋은 컴퓨터 구매하시기 바라며,\n보다 자세한 안내가 필요한 경우 1600-9257 로 연락 부탁드립니다.\n\n감사합니다.',to_date('2018-12-26','YYYY-MM-DD'));
+INSERT INTO p5_notice VALUES (p5_notice_seq.nextval,'컴퓨터 견적을 의뢰하는 경우★필독','고객님의 컴퓨터 견적을 가지고 컴퓨터 제작만 의뢰하는 경우에 대한 세 가지 안내사항을 전달합니다.\n★ 완성됨 상품을 구매하는 경우에 본 내용을 보지 않으셔도 됩니다.\n(1) 출장방문 A/S 를 포함하여 구매하시는 것을 권장합니다. (11,000원~12,000원)\n(2) 박스를 추가 구매하시는 것을 권장합니다. (3,000원)\n(3) 구매를 원하시는 견적과 판매중인 컴퓨터의 견적을 비교해주세요.\n★ 위 내용이 기본 적용되는 곳이 있고, 추가해야 적용되는 곳이 있습니다.\n 해당 사이트의 상세페이지를 확인한 후 추가해주세요.',to_date('2019-01-03','YYYY-MM-DD'));
 SELECT * FROM p5_notice;
 --------------------------------------
