e271236e64b34c424f89ad48b5d2d989acda7c8e
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-05-08 18:09:17 +0900

    포폴

--- funfun/src/funfun/controller/Test.java
+++ funfun/src/funfun/controller/Test.java
@@ -14,7 +14,7 @@ public class Test {
 	@RequestMapping("/test.do")
 	public String test(Model d) {
 		
-		d.addAttribute("test1", "�׽�Ʈ�Դϴ�.");
+		d.addAttribute("test1", "테스트입니다");
 		return "WEB-INF\\views\\test\\testpage.jsp";
 	}
 }
