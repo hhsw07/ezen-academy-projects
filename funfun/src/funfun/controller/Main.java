fab1eb3bb7be03672b1ef35690d3a937431ad426
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-05-10 21:35:51 +0900

    메인상단바 수정

--- funfun/src/funfun/controller/Main.java
+++ funfun/src/funfun/controller/Main.java
@@ -1,5 +1,8 @@
 package funfun.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Main {
 	
+	
+	// http://localhost:5080/funfun/main.do
 	@RequestMapping("/main.do")
-	public String moveMain(Model d) {
+	public String moveMain(Model d, HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("user", "funfun123");
 		
 		return "WEB-INF\\views\\hj_main.jsp";
 	}
