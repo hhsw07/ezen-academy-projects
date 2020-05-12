10202c17bc673c46b9d42530f8d75bce91300949
rlqja914 <60081970+rlqja914@users.noreply.github.com>
2020-05-12 18:13:06 +0900

    asdfdsa

--- funfun/src/funfun/vo/RewardStore.java
+++ funfun/src/funfun/vo/RewardStore.java
@@ -1,77 +1,18 @@
 package funfun.vo;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class RewardStore {
-	private int stoCode;
-	private int proCode;
-	private String regDate;
-	private String subDate;
-	private String stoTitle;
-	private String stoDetail;
-	private String stoImage;
-	private String stoCurr;
-	private String stoCate;
-	private int stoPrice;
-	public int getStoCode() {
-		return stoCode;
-	}
-	public void setStoCode(int stoCode) {
-		this.stoCode = stoCode;
-	}
-	public int getProCode() {
-		return proCode;
-	}
-	public void setProCode(int proCode) {
-		this.proCode = proCode;
-	}
-	public String getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-	public String getSubDate() {
-		return subDate;
-	}
-	public void setSubDate(String subDate) {
-		this.subDate = subDate;
-	}
-	public String getStoTitle() {
-		return stoTitle;
-	}
-	public void setStoTitle(String stoTitle) {
-		this.stoTitle = stoTitle;
-	}
-	public String getStoDetail() {
-		return stoDetail;
-	}
-	public void setStoDetail(String stoDetail) {
-		this.stoDetail = stoDetail;
-	}
-	public String getStoImage() {
-		return stoImage;
-	}
-	public void setStoImage(String stoImage) {
-		this.stoImage = stoImage;
-	}
-	public String getStoCurr() {
-		return stoCurr;
-	}
-	public void setStoCurr(String stoCurr) {
-		this.stoCurr = stoCurr;
-	}
-	public String getStoCate() {
-		return stoCate;
-	}
-	public void setStoCate(String stoCate) {
-		this.stoCate = stoCate;
-	}
-	public int getStoPrice() {
-		return stoPrice;
-	}
-	public void setStoPrice(int stoPrice) {
-		this.stoPrice = stoPrice;
-	}
-	
-	
+	private int sto_code;
+	private int pro_code;
+	private String reg_date;
+	private String sub_date;
+	private String sto_title;
+	private String sto_detail;
+	private String sto_image;
+	private String sto_curr;
+	private String cate_title;
+	private int sto_price;
 }
