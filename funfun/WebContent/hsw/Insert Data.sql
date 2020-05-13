8427c04a3c04f3d77b0a4955e46a555dae22701f
hhsw07 <60540555+hhsw07@users.noreply.github.com>
2020-05-13 13:47:22 +0900

    0513

--- funfun/WebContent/hsw/Insert Data.sql
+++ funfun/WebContent/hsw/Insert Data.sql
@@ -1,12 +1,11 @@
-INSERT INTO rtqna values();
-INSERT INTO rtqna values(rtqna_seq.nextval,0,current_date, '안녕하세요',1100001);
-INSERT INTO rtqna values(rtqna_seq.nextval,1,current_date, '네.안녕하세요',1001);
-INSERT INTO rtqna values(rtqna_seq.nextval,2,current_date, '질문있어요',1100001);
-INSERT INTO rtqna values(rtqna_seq.nextval,0,current_date, '이거 안돼요',1100002);
-INSERT INTO rtqna values(rtqna_seq.nextval,4,current_date, '다시해보세요.',1001);
-INSERT INTO rtqna values(rtqna_seq.nextval,3,current_date, '얼마예요?',1100001);
-INSERT INTO rtqna values(rtqna_seq.nextval,6,current_date, '1만원이요',1001);
-INSERT INTO rtqna values(rtqna_seq.nextval,5,current_date, '그래도 안돼요',1100002);
+INSERT INTO rtqna values(rtqna_seq.nextval,0,current_date, 1100001, '안녕하세요');
+INSERT INTO rtqna values(rtqna_seq.nextval,1,current_date, 1001, '네.안녕하세요');
+INSERT INTO rtqna values(rtqna_seq.nextval,2,current_date,1100001, '질문있어요');
+INSERT INTO rtqna values(rtqna_seq.nextval,0,current_date,1100002, '이거 안돼요');
+INSERT INTO rtqna values(rtqna_seq.nextval,4,current_date,1001, '다시해보세요.');
+INSERT INTO rtqna values(rtqna_seq.nextval,3,current_date,1100001, '얼마예요?');
+INSERT INTO rtqna values(rtqna_seq.nextval,6,current_date,1001, '1만원이요');
+INSERT INTO rtqna values(rtqna_seq.nextval,5,current_date,1100002, '그래도 안돼요');
 
 
 INSERT INTO notice values(notice_seq.nextval,'test01제목','test01내용',sysdate,NULL,'Y',1001);
