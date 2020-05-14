9839e7afda131324d68db5c45d8bc179dd0fa7f0
mkeeeeey <60504392+mkeeeeey@users.noreply.github.com>
2020-05-14 15:47:37 +0900

    android

---
+++ 최민기/Project.java
@@ -0,0 +1,124 @@
+package com.example.funfun;
+
+public class Project {
+    private int pro_code;
+    private int maker_code;
+    private String pro_reg_date;
+    private String pro_sub_date;
+    private String pro_title;
+    private String cate_title;
+    private int pro_target;
+    private String pro_start_date;
+    private String pro_finish_date;
+    private String pro_image;
+    private String pro_story;
+    private String pro_docu;
+    private String pro_keyword;
+    private String pro_curr;
+    private String maker_name;
+
+    public int getPro_code() {
+        return pro_code;
+    }
+    public void setPro_code(int pro_code) {
+        this.pro_code = pro_code;
+    }
+
+    public int getMaker_code() {
+        return maker_code;
+    }
+    public void setMaker_code(int maker_code) {
+        this.maker_code = maker_code;
+    }
+
+    public String getPro_reg_date() {
+        return pro_reg_date;
+    }
+    public void setPro_reg_date(String pro_reg_date) {
+        this.pro_reg_date = pro_reg_date;
+    }
+
+    public String getPro_sub_date() {
+        return pro_sub_date;
+    }
+    public void setPro_sub_date(String pro_sub_date) {
+        this.pro_sub_date = pro_sub_date;
+    }
+
+    public String getPro_title() {
+        return pro_title;
+    }
+    public void setPro_title(String pro_title) {
+        this.pro_title = pro_title;
+    }
+
+    public String getCate_title() {
+        return cate_title;
+    }
+    public void setCate_title(String cate_title) {
+        this.cate_title = cate_title;
+    }
+
+    public int getPro_target() {
+        return pro_target;
+    }
+    public void setPro_target(int pro_target) {
+        this.pro_target = pro_target;
+    }
+
+    public String getPro_start_date() {
+        return pro_start_date;
+    }
+    public void setPro_start_date(String pro_start_date) {
+        this.pro_start_date = pro_start_date;
+    }
+
+    public String getPro_finish_date() {
+        return pro_finish_date;
+    }
+    public void setPro_finish_date(String pro_finish_date) {
+        this.pro_finish_date = pro_finish_date;
+    }
+
+    public String getPro_image() {
+        return pro_image;
+    }
+    public void setPro_image(String pro_image) {
+        this.pro_image = pro_image;
+    }
+
+    public String getPro_story() {
+        return pro_story;
+    }
+    public void setPro_story(String pro_story) {
+        this.pro_story = pro_story;
+    }
+
+    public String getPro_docu() {
+        return pro_docu;
+    }
+    public void setPro_docu(String pro_docu) {
+        this.pro_docu = pro_docu;
+    }
+
+    public String getPro_keyword() {
+        return pro_keyword;
+    }
+    public void setPro_keyword(String pro_keyword) {
+        this.pro_keyword = pro_keyword;
+    }
+
+    public String getPro_curr() {
+        return pro_curr;
+    }
+    public void setPro_curr(String pro_curr) {
+        this.pro_curr = pro_curr;
+    }
+
+    public String getMaker_name() {
+        return maker_name;
+    }
+    public void setMaker_name(String maker_name) {
+        this.maker_name = maker_name;
+    }
+}
---
+++ 최민기/ProjectList.java
@@ -0,0 +1,14 @@
+package com.example.funfun;
+
+import java.util.ArrayList;
+
+public class ProjectList {
+    private ArrayList<Project> plist;
+
+    public ArrayList<Project> getPlist() {
+        return plist;
+    }
+    public void setPlist(ArrayList<Project> plist) {
+        this.plist = plist;
+    }
+}
