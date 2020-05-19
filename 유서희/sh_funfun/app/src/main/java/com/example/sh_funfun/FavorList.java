53f7752156a5acbb88873756f8d29806cf69ad9e
yuseohee27 <59616102+yuseohee27@users.noreply.github.com>
2020-05-19 18:17:38 +0900

    21323

---
+++ 유서희/sh_funfun/app/src/main/java/com/example/sh_funfun/FavorList.java
@@ -0,0 +1,19 @@
+package com.example.sh_funfun;
+
+import java.util.ArrayList;
+
+public class FavorList {
+    private ArrayList<mypageVo> list;
+
+    public FavorList(ArrayList<mypageVo> list) {
+        this.list = list;
+    }
+
+    public ArrayList<mypageVo> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<mypageVo> list) {
+        this.list = list;
+    }
+}
