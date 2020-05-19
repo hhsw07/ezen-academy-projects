b5808b1c25fde7037d12735bba97f885a7c6d274
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-05-19 15:55:32 +0900

    ㄴㅇㅁ

--- funfun/src/funfun/controller/AndroidLoginCtrl.java
+++ funfun/src/funfun/controller/AndroidLoginCtrl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import funfun.service.MainService;
 import funfun.vo.MemberLogin;
@@ -15,18 +16,17 @@ public class AndroidLoginCtrl {
 	MainService service;
 	
 	@RequestMapping("/androidlogin.do")
+	@ResponseBody
 	public String verificationId(MemberLogin m, Model d) {
 		
 		System.out.println("안드로이드 로그인 아이디 비밀번호 검증...");
+		boolean result;
+		String mem_email;
 		
-		if(service.verifyId(m)==true) {
-			d.addAttribute("result", true);
-			d.addAttribute("mem_email", m.getMem_email());
-		} else {
-			d.addAttribute("result", false);
-			d.addAttribute("mem_email", m.getMem_email());
-		}
+		result=service.verifyId(m);
+		mem_email=m.getMem_email();
+		String str="{\"result\":"+result+", \"mem_email\":\""+mem_email+"\"}";
 			
-		return "pageJsonReport";
+		return str;
 	}
 }
