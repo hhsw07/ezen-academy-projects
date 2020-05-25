8e06258d5b8c1e1ccf112a180c6fe77a064db89e
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-05-25 18:19:07 +0900

    dsa

--- funfun/src/funfun/repository/MainRepo.java
+++ funfun/src/funfun/repository/MainRepo.java
@@ -1,14 +1,18 @@
 package funfun.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
 
+import funfun.vo.Favor;
+import funfun.vo.FavorCodeList;
 import funfun.vo.MemberInfo;
 import funfun.vo.MemberLogin;
 
 
 @Repository
 public interface MainRepo {
-	
+
 	public int verifyId(MemberLogin m);
 	
 	public MemberInfo getMemberInfo(String email);
@@ -22,4 +26,10 @@ public interface MainRepo {
 	public int isMaker(int mem_code);
 	
 	public int getMakerCode(int mem_code);
+	
+	public String getNameByEmail(String email);
+	
+	public ArrayList<Integer> getFavorCodeListByEmail(String email);
+	
+	public Favor getFavorByCode(Integer i);
 }
