9c829d04c5274f2225aedbdeb7d8e203cdcadea4
esseone7 <59997115+esseone7@users.noreply.github.com>
2020-05-26 09:46:24 +0900

    1111

--- funfun/src/funfun/service/HT_ProjectRegService.java
+++ funfun/src/funfun/service/HT_ProjectRegService.java
@@ -51,6 +51,7 @@ public class HT_ProjectRegService {
 	}
 	
 <<<<<<< HEAD
+<<<<<<< HEAD
 	public ArrayList<ProRisk> getProRiskList(int pro_code){
 		return dao.getProRiskList(pro_code);
 	}
@@ -71,4 +72,6 @@ public class HT_ProjectRegService {
 >>>>>>> parent of 3e0c965... 1234
 =======
 >>>>>>> parent of 5e57446... 1111
+=======
+>>>>>>> parent of 3e0c965... 1234
 }
