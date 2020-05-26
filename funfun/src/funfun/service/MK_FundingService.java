9a5de59981db6a68efee07dbcae22bded639bfd1
mkeeeeey <60504392+mkeeeeey@users.noreply.github.com>
2020-05-26 18:21:03 +0900

    mk

--- funfun/src/funfun/service/MK_FundingService.java
+++ funfun/src/funfun/service/MK_FundingService.java
@@ -1,9 +1,15 @@
 package funfun.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import funfun.repository.MK_FundingReposi;
 import funfun.vo.Funding;
@@ -25,8 +31,7 @@ public class MK_FundingService {
 		if(sch.getPageSize()==0) {
 			sch.setPageSize(16);
 		}
-		sch.setPageCount((int)(Math.ceil
-				(sch.getCount()/(double)sch.getPageSize())));
+		sch.setPageCount((int)(Math.ceil(sch.getCount()/(double)sch.getPageSize())));
 		if(sch.getCurPage() == 0) {
 			sch.setCurPage(1);
 		}
@@ -65,8 +70,48 @@ public class MK_FundingService {
 		dao.insFavor(proj);
 	}
 	// 프로젝트 신고
+	@Value("${upload}")
+	private String upload; // 업로드 할 위치
+	@Value("${tmpUpload}")
+	private String tmpUpload; // 임시 업로드 위치
 	public void insReport(Report ins) {
+		System.out.println("신고service");
+		for(MultipartFile report:ins.getReport()) {
+			// 물리적 파일 정리
+			upload(report, ins);
+		}
+		System.out.println("파일명:"+ins.getReport_img());
 		dao.insReport(ins);
+	}
+	// 파일정보 등록
+	public void upload(MultipartFile mtf, Report ins) {
+		String fileName = mtf.getOriginalFilename();
+		if(fileName!=null&&!fileName.equals("")) {
+			File tmpFile = new File(tmpUpload+fileName);
+			// 해당 폴더에 동일한 파일이 있으면 삭제 처리
+			if(tmpFile.exists()) {
+				tmpFile.delete();
+			}
+			try {
+				// Stream으로 온 MultipartFile을 실제 파일로 변경처리
+				mtf.transferTo(tmpFile);
+				File orgFile = new File(upload+fileName);
+				// tmp 위치에 있는 파일을 현재 웹서버에 특정한 폴더로 이동
+				// StandardCopyOption.REPLACE_EXISTING : 기존 동일 파일명이 마지막에 올린 파일로 변경처리
+				Files.copy(tmpFile.toPath(), orgFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		ins.setReport_img(fileName);
+	}
+	// 문의 리스트
+	public ArrayList<ProjectQna> inquiryList(int pro_code){
+		return dao.inquiryList(pro_code);
 	}
 	// 문의하기
 	public void inquiry(ProjectQna ins) {
