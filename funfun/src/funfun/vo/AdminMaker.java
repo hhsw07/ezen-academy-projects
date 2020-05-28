ca37fb201a8de27c62867e040e415816a8132315
hhsw07 <60540555+hhsw07@users.noreply.github.com>
2020-05-28 16:36:15 +0900

    발표자료 추가

--- funfun/src/funfun/vo/AdminMaker.java
+++ funfun/src/funfun/vo/AdminMaker.java
@@ -3,148 +3,104 @@ package funfun.vo;
 import java.sql.Date;
 
 public class AdminMaker {
-	// PRO_CODE|MAKER_CODE|PRO_REG_DATE       
-	// PRO_SUB_DATE       |PRO_TITLE                             
-	// CATE_TITLE|PRO_TARGET|PRO_START_DATE     |PRO_FINISH_DATE    |PRO_IMAGE
-	// PRO_STORY  |PRO_DOCU          |PRO_KEYWORD       
-	// PRO_CURR|PRO_MONEY|PRO_AGREE|
+//MAKER_CODE|MEM_CODE|MAKER_PROFILE       
+//|MAKER_TYPE|MAKER_NAME |MAKER_EMAIL    |MAKER_CURR|MAKER_BANKBOOK|
+	/*
+	메이커 구분 - 개인/개인사업자/사업자
+	메이커 상태 - DATE
+	 */
 	
 	private int cnt;
-	private int pro_code;
 	private int maker_code;
-	private Date pro_reg_date;
-	private Date pro_sub_date;
-	private String pro_title;
-	private String cate_title;
-	private int pro_target;
-	private Date pro_start_date;
-	private Date pro_finish_date;
-	private String pro_image;
-	private String pro_story;
-	private String pro_docu;
-	private String pro_keyword;
-	private String pro_curr;
-	private int pro_money;
-	private String pro_agree;
-	
 	private int mem_code;
-	private String mem_name;
+	private String maker_profile;
+	private String maker_type;
+	private String maker_name;
+	private String maker_email;
+	private Date maker_curr;
+	private String maker_bankbook;
 	
+	private String pro_title;
+
 	public int getCnt() {
 		return cnt;
 	}
+
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-	public int getPro_code() {
-		return pro_code;
-	}
-	public void setPro_code(int pro_code) {
-		this.pro_code = pro_code;
-	}
+
 	public int getMaker_code() {
 		return maker_code;
 	}
+
 	public void setMaker_code(int maker_code) {
 		this.maker_code = maker_code;
 	}
-	public Date getPro_reg_date() {
-		return pro_reg_date;
-	}
-	public void setPro_reg_date(Date pro_reg_date) {
-		this.pro_reg_date = pro_reg_date;
-	}
-	public Date getPro_sub_date() {
-		return pro_sub_date;
-	}
-	public void setPro_sub_date(Date pro_sub_date) {
-		this.pro_sub_date = pro_sub_date;
-	}
-	public String getPro_title() {
-		return pro_title;
-	}
-	public void setPro_title(String pro_title) {
-		this.pro_title = pro_title;
-	}
-	public String getCate_title() {
-		return cate_title;
-	}
-	public void setCate_title(String cate_title) {
-		this.cate_title = cate_title;
-	}
-	public int getPro_target() {
-		return pro_target;
-	}
-	public void setPro_target(int pro_target) {
-		this.pro_target = pro_target;
-	}
-	public Date getPro_start_date() {
-		return pro_start_date;
-	}
-	public void setPro_start_date(Date pro_start_date) {
-		this.pro_start_date = pro_start_date;
-	}
-	public Date getPro_finish_date() {
-		return pro_finish_date;
-	}
-	public void setPro_finish_date(Date pro_finish_date) {
-		this.pro_finish_date = pro_finish_date;
-	}
-	public String getPro_image() {
-		return pro_image;
-	}
-	public void setPro_image(String pro_image) {
-		this.pro_image = pro_image;
-	}
-	public String getPro_story() {
-		return pro_story;
-	}
-	public void setPro_story(String pro_story) {
-		this.pro_story = pro_story;
-	}
-	public String getPro_docu() {
-		return pro_docu;
-	}
-	public void setPro_docu(String pro_docu) {
-		this.pro_docu = pro_docu;
-	}
-	public String getPro_keyword() {
-		return pro_keyword;
-	}
-	public void setPro_keyword(String pro_keyword) {
-		this.pro_keyword = pro_keyword;
-	}
-	public String getPro_curr() {
-		return pro_curr;
-	}
-	public void setPro_curr(String pro_curr) {
-		this.pro_curr = pro_curr;
-	}
-	public int getPro_money() {
-		return pro_money;
-	}
-	public void setPro_money(int pro_money) {
-		this.pro_money = pro_money;
-	}
-	public String getPro_agree() {
-		return pro_agree;
-	}
-	public void setPro_agree(String pro_agree) {
-		this.pro_agree = pro_agree;
-	}
+
 	public int getMem_code() {
 		return mem_code;
 	}
+
 	public void setMem_code(int mem_code) {
 		this.mem_code = mem_code;
 	}
-	public String getMem_name() {
-		return mem_name;
+
+	public String getMaker_profile() {
+		return maker_profile;
 	}
-	public void setMem_name(String mem_name) {
-		this.mem_name = mem_name;
+
+	public void setMaker_profile(String maker_profile) {
+		this.maker_profile = maker_profile;
 	}
-	
+
+	public String getMaker_type() {
+		return maker_type;
+	}
+
+	public void setMaker_type(String maker_type) {
+		this.maker_type = maker_type;
+	}
+
+	public String getMaker_name() {
+		return maker_name;
+	}
+
+	public void setMaker_name(String maker_name) {
+		this.maker_name = maker_name;
+	}
+
+	public String getMaker_email() {
+		return maker_email;
+	}
+
+	public void setMaker_email(String maker_email) {
+		this.maker_email = maker_email;
+	}
+
+	public Date getMaker_curr() {
+		return maker_curr;
+	}
+
+	public void setMaker_curr(Date maker_curr) {
+		this.maker_curr = maker_curr;
+	}
+
+	public String getMaker_bankbook() {
+		return maker_bankbook;
+	}
+
+	public void setMaker_bankbook(String maker_bankbook) {
+		this.maker_bankbook = maker_bankbook;
+	}
+
+	public String getPro_title() {
+		return pro_title;
+	}
+
+	public void setPro_title(String pro_title) {
+		this.pro_title = pro_title;
+	}
 	
 	
 	
