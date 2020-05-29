23f39d2685684f8bcbb52e2b9f286d9c245a616e
mkeeeeey <60504392+mkeeeeey@users.noreply.github.com>
2020-05-29 18:18:49 +0900

    report

--- funfun/src/funfun/vo/Report.java
+++ funfun/src/funfun/vo/Report.java
@@ -11,10 +11,20 @@ public class Report {
 	private String report_detail; // 신고내용
 	private String report_img; // 첨부파일
 	private String report_date; // 신고날짜
-	
 	private String pro_title; //  프로젝트 명
 	private String mem_name; // 회원이름
 	
+	// 2. 페이지
+	private int count; 		// 총 데이터 건수
+	private int pageSize; 	// 한번에 보여줄 페이지 크기..
+	private int pageCount; 	// 총 페이지 수. count/pageSize
+	private int curPage; 	// 클릭한 현재 페이지번호
+	private int start;		// 화면에 보여줄 페이지의 시작번호
+	private int end;		// 화면에 보여줄 페이지의 마지막번호
+	// 3. 블럭 처리.
+	private int blocksize;  // 한번에 보여줄 block의 크기
+	private int startBlock;	// block의 시작번호
+	private int endBlock;	// block의 마지막번호
 	
 	public int getReport_code() {
 		return report_code;
@@ -63,6 +73,60 @@ public class Report {
 	}
 	public void setMem_name(String mem_name) {
 		this.mem_name = mem_name;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public int getCurPage() {
+		return curPage;
+	}
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public int getBlocksize() {
+		return blocksize;
+	}
+	public void setBlocksize(int blocksize) {
+		this.blocksize = blocksize;
+	}
+	public int getStartBlock() {
+		return startBlock;
+	}
+	public void setStartBlock(int startBlock) {
+		this.startBlock = startBlock;
+	}
+	public int getEndBlock() {
+		return endBlock;
+	}
+	public void setEndBlock(int endBlock) {
+		this.endBlock = endBlock;
 	}
 	
 
